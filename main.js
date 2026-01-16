ebefa174a30aa639f107233e43c232c77ca1d140
aj <amjamal4050@gmail>
2026-01-16 21:57:07 +0300

    adding draw and board adn other for javascripte and fix div

---
+++ main.js
@@ -0,0 +1,40 @@
+const playerx = document.querySelector(".xplayer");
+const playero = document.querySelector(".oplayer");
+const cell = document.querySelectorAll(".cell");
+const title =document.querySelectorAll('title');
+
+let player = "X";
+let pause = false;
+const boardgame = ["", "", "", "", "", "", "", "", ""];
+
+const winbox = [
+ [0,1,2],[3,4,5],[6,7.8]
+ [0,3,6],[1,4,7],[2,7,8]
+ [0,4,7] ,[2,3,6]
+ ]
+
+function changeplayer() {
+    player = (player == 'X') ? 'O' : 'X'
+}
+
+function draw() {
+    title.textContent = 'Draw!'
+    pause = true
+}
+
+function checkWinner() {
+    for (const [a, b, c] of wincheck) {
+        if (boardgame[a] == player &&
+            boardgame[b] == player &&
+            boardgame[c] == player
+        ) {
+        }
+    }
+    
+    if (boardgame.every(cell => cell != '')) {
+        draw()
+        return true
+    }
+    return false
+}
+
