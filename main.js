8c3dbf9b3fc0be68844c18dfa274659d8fb3dacd
aj <amjamal4050@gmail>
2026-01-16 23:27:42 +0300

    Adding functionly to the tic tac toe game

--- main.js
+++ main.js
@@ -1,40 +1,69 @@
 const playerx = document.querySelector(".xplayer");
 const playero = document.querySelector(".oplayer");
 const cell = document.querySelectorAll(".cell");
-const title =document.querySelectorAll('title');
+const title = document.querySelector("#title");
 
 let player = "X";
 let pause = false;
 const boardgame = ["", "", "", "", "", "", "", "", ""];
 
 const winbox = [
- [0,1,2],[3,4,5],[6,7.8]
- [0,3,6],[1,4,7],[2,7,8]
- [0,4,7] ,[2,3,6]
- ]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7,8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 7, 8],
+  [0, 4, 7],
+  [2, 3, 6],
+];
 
 function changeplayer() {
-    player = (player == 'X') ? 'O' : 'X'
+  player = player == "X" ? "O" : "X";
 }
 
 function draw() {
-    title.textContent = 'Draw!'
-    pause = true
+  title.textContent = "Draw!";
+  pause = true;
 }
 
-function checkWinner() {
-    for (const [a, b, c] of wincheck) {
-        if (boardgame[a] == player &&
-            boardgame[b] == player &&
-            boardgame[c] == player
-        ) {
-        }
+function checkwinner() {
+  for (const [a, b, c] of winbox) {
+    if (
+      boardgame[a] == player &&
+      boardgame[b] == player &&
+      boardgame[c] == player
+    ) {
+      winneris([a, b, c]);
+      return true;
     }
-    
-    if (boardgame.every(cell => cell != '')) {
-        draw()
-        return true
-    }
-    return false
+  }
+
+  if (boardgame.every((cell) => cell != "")) {
+    draw();
+    return true;
+  }
+  return false;
 }
 
+function winneris(winner) {
+  title.textContent = `${player} Wins!`;
+  pause = true;
+
+  winner.forEach((index) => {
+    cell[index].style.backgroundColor = "gold";
+  });
+}
+
+cell.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    if (element.textContent === "" && !pause) {
+      element.textContent = player;
+      boardgame[index] = player;
+      element.classList.add(player.toLowerCase());
+      if (!checkwinner()) {
+        changeplayer();
+      }
+    }
+  });
+});
